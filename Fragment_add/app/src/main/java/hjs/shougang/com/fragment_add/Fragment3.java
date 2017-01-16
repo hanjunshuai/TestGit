606311f05ebc84fc9a33f9fd430c10c2081248ed
hanjunshuai <1433452759@qq.com>
2017-01-16 13:30:23 +0800

    hjs

---
+++ Fragment_add/app/src/main/java/hjs/shougang/com/fragment_add/Fragment3.java
@@ -0,0 +1,29 @@
+package hjs.shougang.com.fragment_add;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2017/1/16 0016.
+ */
+
+public class Fragment3 extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        view = inflater.inflate(R.layout.fragment3, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
